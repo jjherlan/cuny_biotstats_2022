04fc9205fa1e6f4ffc403593af2ae1ec3eb80ca0
jjherlan <75087560+jjherlan@users.noreply.github.com>
2022-11-05 19:45:39 -0400

    commit and push, 1945, 11/5/2022

---
+++ midterm_review.R
@@ -0,0 +1,13 @@
+0.769
+
+1.96
+
+# Use this for the Agresti-Coull method of confidence interval
+# Left
+0.769-(1.96*sqrt((0.769*(1-0.769))/(169+4)))
+
+#Right
+0.769-(1.96*sqrt((0.769*(1-0.769))/(169+4)))
+
+
+0.769-(1.96*sqrt((0.769*(1-0.769))/(169+4)))
